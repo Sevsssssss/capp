736ffdfc4bd0cccbb3a7708c1d46b119b55bf300
returnjosh <supremojoshua22@gmail.com>
2022-03-18 21:29:12 +0800

    update inst section

--- src/components/InstructionSection.vue
+++ src/components/InstructionSection.vue
@@ -1,16 +1,16 @@
 <template>
 <!-- Apply for Section -->
-<div class="flex flex-col justify-center space-y-5 p-16 text-black-300 ">
+<div class="flex flex-col space-y-5 p-12 text-black-200 ">
     <!-- Title -->
-    <div class="space-y-5 ">
-        <div class="font-semibold lead-paragraph">How to Apply?</div>
-        <p class="justify-center ml-32 mr-32 label-text">Use these cards when you want to display content with an image, such as a blog post or product. They’re built with CSS grid to enable the 3-column layout. When you select the “Cards Grid Container,” you’ll see a red icon on the top </p>
+    <div class="space-y-4">
+        <div class="font-semibold text-2xl">HOW TO APPLY?</div>
+        <p class="mx-32 label-text">Use these cards when you want to display content with an image, such as a blog post or product. They’re built with CSS grid to enable the 3-column layout. When you select the “Cards Grid Container,” you’ll see a red icon on the top </p>
     </div>
     <!-- Details -->
     <div class="grid grid-cols-4 gap-4 ">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-grey-400 p-8 text-left bg-brand-white shad " v-for="apply in applyfor" :key="apply.id">
+        <div class="max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-grey-400 p-8 text-left bg-brand-white shad " v-for="apply in applyfor" :key="apply.id">
             <div class="pb-4 label-xl">{{apply.title}}</div>
-            <p class="label-text ove">{{apply.info.substring(0, 80) + '..'}}<span class="text-brand-darkblue hover:text-brand-lightblue"> more.. </span> </p>
+            <p class="label-text">{{apply.info.substring(0, 80) + '..'}}<span class="text-brand-darkblue hover:text-blue-100"> more.. </span> </p>
         </div>
     </div>
 </div>
