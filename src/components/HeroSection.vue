86604ffe11fe105b7dec8ed301baa04244ea570b
returnjosh <supremojoshua22@gmail.com>
2022-03-18 21:30:22 +0800

    update hero toggle btn

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -28,7 +28,7 @@
                 </div>
             </div>
             <!-- Section for Login -->
-            <div class="flex-col space-y-10 w-full max-w-md mt-10 bg-brand-white hover:shadow-grey-200 text-black-300 rounded-lg " v-if="!showModal">
+            <div class="flex-col space-y-10 w-full max-w-md mt-10 bg-brand-white hover:shadow-grey-200 text-black-300 rounded-lg " v-if="showModal">
                 <div class="flex justify-end pt-5 pr-5 " @click="showModal = false">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
