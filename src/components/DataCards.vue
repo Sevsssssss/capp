41cddc536b957d692b695805a5043dad4d66462d
Xave <dxbondad36@gmail.com>
2022-03-25 04:32:02 +0800

    UI Updates on Application and HEI

---
+++ src/components/DataCards.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="grid grid-cols-4">
+    <div class="hei-cont space-x-3 bg-brand-white shadow-lg rounded-lg m-3 p-6" v-for="data in datas" :key="data">
+        <div class="flex flex-col justify-between text-left">
+            <div class="flex flex-row">
+                <div :class="'homeIcon ' + data.color" class="mr-3">
+                    <svg class="icon" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z" />
+                    </svg>
+                </div>
+                <div class="label-text font-semibold" style="color: #8fa0b9">
+                    {{ data.title }}
+                </div>
+            </div>
+            <div class="hei-data">{{ data.num }}</div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "DataCards",
+    props: ["datas"],
+    data() {
+        return {
+            colors: [{
+                    color: "orange",
+                },
+                {
+                    color: "blue",
+                },
+                {
+                    color: "violet",
+                },
+                {
+                    color: "green",
+                },
+                {
+                    color: "pink",
+                },
+            ],
+        };
+    },
+    components: {},
+    methods: {},
+};
+</script>
+
+<style>
+</style>
