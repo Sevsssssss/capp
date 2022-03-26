cdfef5bc6d52ea9e7fdbac5515087b2ec96bc19b
Xave <dxbondad36@gmail.com>
2022-03-27 03:44:15 +0800

    NEW UI Updates on Application and HEI

--- src/views/RqatView.vue
+++ src/views/RqatView.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="" style="padding: 10px">
+    <div class="top-row flex flex-row" style="justify-content: space-between;">
+            <TableTopLeft/>
+
+            <div class="right-side flex flex-row">
+                <div class="month-sort flex flex-row">
+                    <select class="select select-ghost select-sm w-full max-w-xs" style="outline: none">
+                        <option disabled selected>Sort by type</option>
+                        <option>Private</option>
+                        <option>State Univeristies</option>
+                        <option>Local Universities</option>
+                        <option>Others</option>
+                    </select>
+                </div>
+
+                <button @click="addHei()" class="btn btn-primary1 bg-brand-darkblue">
+                    <div class="flex flex-row add-hei-content">
+                        <svg style="fill:white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11H7v2h4v4h2v-4h4v-2h-4V7h-2v4z"/></svg>
+                        <div class="btn-text">Add HEI</div>
+                    </div>
+                </button>
+            </div>
+        </div>
     <DataTables :headers="headers" :tables="tables" />
   </div>
 </template>
 
 <script>
 import DataTables from "@/components//DataTables.vue";
+import TableTopLeft from "@/components//TableTopLeft.vue";
 
 export default {
   name: "ApplicationView",
@@ -82,6 +105,7 @@ export default {
   },
   components: {
     DataTables,
+    TableTopLeft,
   },
 };
 </script>
