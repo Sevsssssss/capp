a01e4dc5386d45693c946d118e39972e21ae21b9
Xave <dxbondad36@gmail.com>
2022-03-29 15:46:15 +0800

    Testing Error Handling HEI

--- src/views/AddHeiView.vue
+++ src/views/AddHeiView.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="main-page flex justify-center items-center p-10">
     <div class=" card justify-center items-center space-x-3 bg-brand-white shadow-lg rounded-lg m-3 p-6">
-        <div class="card-body">
+        <form @submit.prevent="handleSubmit" class="card-body">
             <div class="flex flex-row">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z" /></svg>
                 </div>
                 <h2 class="card-title pl-3">ADD HEI ACCOUNT</h2>
             </div>
@@ -14,7 +16,10 @@
                 <label class="label">
                     <span class="label-text">HEI Name</span>
                 </label>
-                <input type="text" placeholder="Enter HEI’s name" class="input input-bordered w-full" />
+                <input type="text" placeholder="Enter HEI’s name" class="input input-bordered w-full" v-model="name" />
+                <label class="label">
+                    <span class="label-text-alt" v-if="nameError"> {{nameError}}</span>
+                </label>
             </div>
 
             <div class="form-control w-full">
@@ -58,9 +63,9 @@
 
             <div class="flex flex-row pt-5">
                 <button class="btn btn-margin btn-wide btn-outline">Cancel</button>
-                <button class="btn btn-margin btn-wide  bg-brand-darkblue hover:bg-brand-blue">Add HEI</button>
+                <button class="btn btn-margin btn-wide submit  bg-brand-darkblue hover:bg-brand-blue">Add HEI</button>
             </div>
-        </div>
+        </form>
     </div>
 </div>
 </template>
@@ -70,6 +75,8 @@ export default {
     name: "AddHeiView",
     data() {
         return {
+            name: "",
+            nameError: "",
             heis: [{
                     title: "STATE UNIVERSITIES AND COLLEGES",
                 },
@@ -86,6 +93,12 @@ export default {
         };
     },
     components: {},
+    methods: {
+        handleSubmit() {
+            // validate if there is a name
+            this.nameError = this.name.length > 0 ? '' : 'HEI Name is Required'
+        }
+    },
 };
 </script>
 
