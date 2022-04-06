75f30483a5517da148fe4a072158560e3ec1e31d
returnjosh <supremojoshua22@gmail.com>
2022-04-06 14:48:39 +0800

    added page edit file

--- src/views/HEI/HEI_Application.vue
+++ src/views/HEI/HEI_Application.vue
@@ -63,12 +63,10 @@
                         </div>
                     </td>
                     <td>
-                        <div class="flex flex-row center">
-                            <button class="" @click="goedit()">
-                                <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" />
-                                </svg>
-                            </button>
+                        <div>
+                            <svg @click="goedit()" class="hover:text-blue-500" style="width: 24px; height: 24px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" />
+                            </svg>
                         </div>
                     </td>
                 </tr>
@@ -114,6 +112,11 @@
 <script>
 export default {
     name: "ApplicationView",
+    methods: {
+        goedit() {
+            this.$router.push("/HEIapplication/edit");
+        }
+    },
     data() {
         return {
             headers: [{
@@ -183,9 +186,7 @@ export default {
                     status: "COMPLETED",
                 },
             ],
-            goedit() {
-               this.$router.push("/HEIapplication/edit");
-            }
+
         };
     },
 };
