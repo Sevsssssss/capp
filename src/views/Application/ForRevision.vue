fb24073cdc7c11d9ae29314e7ca29bf6b65b7097
xxttian <csaquillo@gbox.adnu.edu.ph>
2022-04-11 22:04:31 +0800

    replaced the radio button with check and wrong icon

--- src/views/Application/ForRevision.vue
+++ src/views/Application/ForRevision.vue
@@ -18,10 +18,19 @@
                         {{table.file}}
                     </td>
                     <td class="px-6 py-4">
-                        <input type="radio"  :id="'approved ' + table.id" :value="'approved ' + table.id" class="radio" v-model="statusShow">
+                        <svg class="check" width="24" height="24">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.997-4L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z" />
+                        </svg>
+                        <!--<input type="radio"  :id="'approved ' + table.id" :value="'approved ' + table.id" class="radio" v-model="statusShow">-->
                     </td>
                     <td class="px-6 py-4">
-                        <input type="radio" :id="'disapproved ' + table.id" :value="'disapproved ' + table.id" class="radio" v-model="statusShow">
+                        <svg class="wrong" width="24" height="24">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z" />
+                        </svg>
+                        
+                        <!--<input type="radio" :id="'disapproved ' + table.id" :value="'disapproved ' + table.id" class="radio" v-model="statusShow">-->
                     </td>
                     <td class="px-6 py-4">
                         {{table.comment}}
