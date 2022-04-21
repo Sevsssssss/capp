0db59363d8ec090bc3c8a44260a714cff2f3ff7f
Sevsssssss <sevsarate28@gmail.com>
2022-04-21 15:11:45 +0800

    Hei edit route fix

--- src/views/HEI/EditApplication.vue
+++ src/views/HEI/EditApplication.vue
@@ -1,28 +1,27 @@
 <template>
-  <div>
+<div>
     <div class="m-3">
-      <div class="flex flex-row justify-between">
-        <div class="flex flex-col">
-          <div class="p-4 text-left space-y-3">
-            <div>Type: {{ type }}</div>
-            <div>Date Applied: {{ dateApplied }}</div>
-          </div>
+        <div class="flex flex-row justify-between">
+            <div class="flex flex-col">
+                <div class="p-4 text-left space-y-3">
+                    <div>Type: {{ type }}</div>
+                    <div>Date Applied: {{ dateApplied }}</div>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <div class="text-left p-4 pr-6 space-y-3">
+                    <span>Status: {{ status }}</span>
+                </div>
+                <!-- <p>ID Number: {{id}}</p> -->
+            </div>
         </div>
-        <div class="flex flex-col">
-          <div class="text-left p-4 pr-6 space-y-3">
-            <span>Status: {{ status }}</span>
-          </div>
-          <!-- <p>ID Number: {{id}}</p> -->
-        </div>
-      </div>
     </div>
     <div class="m-3">
-      <div class="flex justify-between p-4">
-        <div class="">
-          <label for="table-search" class="sr-only">Search</label>
-          <div class="relative mt-1">
-            <div
-              class="
+        <div class="flex justify-between p-4">
+            <div class="">
+                <label for="table-search" class="sr-only">Search</label>
+                <div class="relative mt-1">
+                    <div class="
                 absolute
                 inset-y-0
                 left-0
@@ -30,25 +29,12 @@
                 items-center
                 pl-3
                 pointer-events-none
-              "
-            >
-              <svg
-                class="w-5 h-5 text-gray-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="search"
-              class="
+              ">
+                        <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" id="search" class="
                 bg-gray-50
                 border border-gray-300
                 text-gray-900 text-sm
@@ -58,15 +44,11 @@
                 w-80
                 pl-10
                 p-2.5
-              "
-              placeholder="Search for items"
-            />
-          </div>
-        </div>
-        <div>
-          <button
-            type="button"
-            class="
+              " placeholder="Search for items" />
+                </div>
+            </div>
+            <div>
+                <button type="button" class="
               text-white
               bg-blue-700
               hover:bg-blue-800
@@ -78,48 +60,35 @@
               py-2.5
               mr-2
               mb-2
-            "
-          >
-            Submit
-          </button>
+            ">
+                    Submit
+                </button>
+            </div>
         </div>
-      </div>
 
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th
-                v-for="header in headers"
-                :key="header"
-                scope="col"
-                class="px-6 py-3"
-              >
-                {{ header.title }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="table in searchHEI"
-              :key="table"
-              class="bg-white border-b"
-            >
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900">
-                {{ table.credential }}
-              </th>
-              <td class="px-6 py-4">
-                {{ table.file }}
-              </td>
-              <td class="px-6 py-4">
-                {{ table.comment }}
-              </td>
-              <td class="flex items-end px-6 py-4">
-                <a v-if="table.comment === ''"></a>
-                <input
-                  v-if="table.comment != ''"
-                  accept=".pdf,.doc"
-                  class="
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th v-for="header in headers" :key="header" scope="col" class="px-6 py-3">
+                            {{ header.title }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="table in searchHEI" :key="table" class="bg-white border-b">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900">
+                            {{ table.credential }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ table.file }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ table.comment }}
+                        </td>
+                        <td class="flex items-end px-6 py-4">
+                            <a v-if="table.comment === ''"></a>
+                            <input v-if="table.comment != ''" accept=".pdf,.doc" class="
                     block
                     w-full
                     text-sm text-gray-900
@@ -128,97 +97,90 @@
                     border border-gray-300
                     cursor-pointer
                     focus:outline-none focus:border-transparent
-                  "
-                  aria-describedby="user_avatar_help"
-                  id="user_avatar"
-                  type="file"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+                  " aria-describedby="user_avatar_help" id="user_avatar" type="file" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import Parse from 'parse'
 export default {
-  props: ["id", "HeiName", "type", "status", "dateApplied"],
-  name: "EditHEIapplication",
-  data() {
-    return {
-      // id: this.$route.params.id,
-      show: false,
-      headers: [
-        {
-          title: "CREDENTIALS",
-        },
-        {
-          title: "FILES",
-        },
-        {
-          title: "COMMENTS",
-        },
-      ],
-      tables: [
-        {
-          credential: "Articles of Incorporation and By-Laws..",
-          file: "ArticlesofInc.pdf",
-          comment:
-            "Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit.",
-        },
-        {
-          credential: "Copy(ies) of Transfer of Certificate(s) Title (TCT)",
-          file: "Copy(ies)ofTransferof.pdf",
-          comment: "",
-        },
-        {
-          credential: "Ownership of School Building",
-          file: "OwnershipofSchoolBuilding.pdf",
-          comment: "",
-        },
-        {
-          credential: "Campus Development and Landscaping Plan",
-          file: "CampusDevelopme.pdf",
-          comment: "",
-        },
-      ],
-      search: "",
-    };
-  },
-
-  computed: {
-    searchHEI() {
-      return this.tables.filter((p) => {
-        return (
-          p.credential.toLowerCase().indexOf(this.search.toLowerCase()) != -1
-        );
-      });
+    props: ["id", "HeiName", "type", "status", "dateApplied"],
+    name: "EditHEIapplication",
+    data() {
+        return {
+            // id: this.$route.params.id,
+            show: false,
+            headers: [{
+                    title: "CREDENTIALS",
+                },
+                {
+                    title: "FILES",
+                },
+                {
+                    title: "COMMENTS",
+                },
+            ],
+            tables: [{
+                    credential: "Articles of Incorporation and By-Laws..",
+                    file: "ArticlesofInc.pdf",
+                    comment: "Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit.",
+                },
+                {
+                    credential: "Copy(ies) of Transfer of Certificate(s) Title (TCT)",
+                    file: "Copy(ies)ofTransferof.pdf",
+                    comment: "",
+                },
+                {
+                    credential: "Ownership of School Building",
+                    file: "OwnershipofSchoolBuilding.pdf",
+                    comment: "",
+                },
+                {
+                    credential: "Campus Development and Landscaping Plan",
+                    file: "CampusDevelopme.pdf",
+                    comment: "",
+                },
+            ],
+            search: "",
+        };
     },
-  },
-  mounted: async function () {
-    console.log("hi");
-    // THIS LINES OF CODE CHECKS IF THE USER HAS A PERMISSION TO ACCESS THIS ROUTE
-    const AccessTypes = Parse.Object.extend("AccessTypes");
-    const query = new Parse.Query(AccessTypes);
-    query.equalTo("name", Parse.User.current().get("access_type"));
 
-    const querResult = await query.find();
-    var homeType = querResult[0].get("hometype");
-    var flag = 0;
-    if (homeType === "/home") {
-      flag = 1;
-    }
-    if (flag === 0) {
-      this.$router.push("/403");
-    } else {
-      console.log("Hi!, You have permission to access this Page");
-      //INSERT HERE MOUNTED ARGUMENTS FOR THIS COMPONENT
-      //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    }
-  },
+    computed: {
+        searchHEI() {
+            return this.tables.filter((p) => {
+                return (
+                    p.credential.toLowerCase().indexOf(this.search.toLowerCase()) != -1
+                );
+            });
+        },
+    },
+    mounted: async function () {
+        console.log("hi");
+        // THIS LINES OF CODE CHECKS IF THE USER HAS A PERMISSION TO ACCESS THIS ROUTE
+        const AccessTypes = Parse.Object.extend("AccessTypes");
+        const query = new Parse.Query(AccessTypes);
+        query.equalTo("name", Parse.User.current().get("access_type"));
+
+        const querResult = await query.find();
+        var homeType = querResult[0].get("hometype");
+        var flag = 0;
+        if (homeType === "/HEIhome") {
+            flag = 1;
+        }
+        if (flag === 0) {
+            this.$router.push("/403");
+        } else {
+            console.log("Hi!, You have permission to access this Page");
+            //INSERT HERE MOUNTED ARGUMENTS FOR THIS COMPONENT
+            //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        }
+    },
 };
 </script>
 
