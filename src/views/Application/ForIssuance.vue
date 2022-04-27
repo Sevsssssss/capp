ed91296fdf17d86a9da1091af8d67213ffb5305d
returnjosh <supremojoshua22@gmail.com>
2022-04-27 18:20:10 +0800

    update for issuance

--- src/views/Application/ForIssuance.vue
+++ src/views/Application/ForIssuance.vue
@@ -1,29 +1,31 @@
 <template>
-<div class="m-3">
-    <div class="overflow-x-auto shadow-lg rounded-lg m-2">
-        <div class="p-10 h-full flex flex-col justify-center items-center bg-white">
-            <span class="text-2xl m-5">Upload File</span>
-            <div @drop.prevent="drop" @change="selectedFile" @dragenter.prevent="toggleActive" @dragleave.prevent="toggleActive" @dragover.prevent :class="{ 'active-dropzone': active }" class="dropzone">
-                <span>Drag or Drop File</span>
-                <span>OR</span>
-                <label for="dropzoneFile">Select File</label>
-                <input type="file" id="dropzoneFile" class="dropzoneFile" />
-            </div>
-            <span class="mt-5 font-semibold">File: <span class="text-brand-blue/50">{{ dropzoneFile.name }}</span></span>
-            <div class="flex flex-row center pt-10">
-                <div class="pr-5">
-                    <button @click="$router.go(-1)" type="button" class="btn text-blue-700 bg-transparent border border-blue-700 hover:bg-white" data-dismiss-target="#alert-additional-content-1" aria-label="Close">
-                        <div>Dismiss</div>
-                    </button>
-                </div>
-                <div>
-                    <label for="" class="btn modal-button border-none text-white bg-blue-700 hover:bg-blue-800">
-                        Complete</label>
-                </div>
-
-            </div>
+<div class="my-3 mx-2">
+    <div class="overflow-x-auto shadow-lg rounded-lg">
+    <div class="py-5 h-full flex flex-col justify-center items-center bg-white">
+        <span class="text-2xl m-5">Upload File</span>
+        <div @drop.prevent="drop" @change="selectedFile" @dragenter.prevent="toggleActive" @dragleave.prevent="toggleActive" @dragover.prevent :class="{ 'active-dropzone': active }" class="dropzone">
+            <span>Drag or Drop File</span>
+            <span>OR</span>
+            <label for="dropzoneFile">Select File</label>
+            <input type="file" id="dropzoneFile" class="dropzoneFile" />
         </div>
+        <span class="m-3 font-semibold">File: <span class="text-brand-blue/50">{{ dropzoneFile.name }}</span></span>
+
     </div>
+
+</div>
+<div class="flex flex-row center py-10">
+    <div class="pr-5">
+        <button @click="$router.go(-1)" type="button" class="btn text-blue-700 bg-transparent border border-blue-700 hover:bg-white" data-dismiss-target="#alert-additional-content-1" aria-label="Close">
+            <div>Dismiss</div>
+        </button>
+    </div>
+    <div>
+        <label for="" class="btn modal-button border-none text-white bg-blue-700 hover:bg-blue-800">
+            Complete</label>
+    </div>
+
+</div>
 </div>
 </template>
 
@@ -68,6 +70,7 @@ export default {
     background-color: #fff;
     transition: 0.3s ease all;
 }
+
 .dropzone label {
     padding: 8px 12px;
     color: #fff;
@@ -75,14 +78,17 @@ export default {
     transition: 0.3s ease all;
     border-radius: 5px;
 }
+
 .dropzone input {
     display: none;
 }
+
 .active-dropzone {
     color: #fff;
     border-color: #fff;
     background-color: #C4C4C4;
 }
+
 .active-dropzone label {
     background-color: #fff;
     color: #41b883;
