288305c5f0bd5b7c54d699944a6a1991e7630f97
Xave <dxbondad36@gmail.com>
2022-04-27 09:34:35 +0800

    Updates for UI-REPO

--- src/views/CHED/Disciplines/DisciplinesView.vue
+++ src/views/CHED/Disciplines/DisciplinesView.vue
@@ -46,26 +46,7 @@
     </div>
 </div>
 <div v-else class="p-3">
-    <div class="grid xxl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3">
-        <div class="bg-brand-white shadow-md rounded-md m-3 p-4" v-for="data in datas" :key="data">
-            <div class="flex flex-col justify-between text-left">
-                <div class="flex flex-row">
-                    <div :class="'homeIcon ' + data.color" class="mr-3">
-                        <svg class="icon" width="24" height="24">
-                            <path fill="none" d="M0 0h24v24H0z" />
-                            <path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z" />
-                        </svg>
-                    </div>
-                    <div class="text-sm font-semibold" style="color: #8fa0b9">
-                        {{ data.title }}
-                    </div>
-                </div>
-                <div class="text-2xl text-right font-semibold text-grey-300">
-                    {{ data.num }}
-                </div>
-            </div>
-        </div>
-    </div>
+
     <!-- Table -->
     <div class="overflow-x-auto shadow-lg rounded-lg m-2">
         <!-- Table header -->
