0306b13ba1098cdf6cc146f4693c576d479c2b15
TheToinkz <jbanday@gbox.adnu.edu.ph>
2022-05-16 11:26:24 +0800

    heiTypes edit done

--- src/views/CHED/HeiTypes/HeiTypeView.vue
+++ src/views/CHED/HeiTypes/HeiTypeView.vue
@@ -56,7 +56,7 @@
                         {{ i.Name }}
                     </td>
                     <td class="px-6 py-4 flex flex-row justify-end">
-                        <label for="editHei" href="#" class="font-medium text-blue-600 hover:underline pr-3" @click="selectHei(i.id)">Edit</label>
+                        <label for="editHei" href="#" class="font-medium text-blue-600 hover:underline pr-3" @click="selectHeiType(i.id)">Edit</label>
                         <label for="deleteFunc" class="hover:text-brand-red/60 self-center">
                             <svg style="width: 20px; height: 20px" viewBox="0 0 24 24" @click="selectedHeiDelete(i.id)">
                                 <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
