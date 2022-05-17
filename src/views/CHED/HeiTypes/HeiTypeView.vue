f09cb134e95c41c7e90109e16e0e10ba4a5b469f
Xave <dxbondad36@gmail.com>
2022-05-17 23:24:50 +0800

    Updates on RQAT EVALUATE

--- src/views/CHED/HeiTypes/HeiTypeView.vue
+++ src/views/CHED/HeiTypes/HeiTypeView.vue
@@ -58,7 +58,7 @@
                     <td class="px-6 py-4 flex flex-row justify-end">
                         <label for="editHei" href="#" class="font-medium text-blue-600 hover:underline pr-3" @click="selectHeiType(i.id)">Edit</label>
                         <label for="deleteFunc" class="hover:text-brand-red/60 self-center">
-                            <svg style="width: 20px; height: 20px" viewBox="0 0 24 24" @click="selectedHeiDelete(i.id)">
+                            <svg style="width: 20px; height: 20px" viewBox="0 0 24 24" @click="selectedHeiTypeDelete(i.id)">
                                 <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
                             </svg>
                         </label>
