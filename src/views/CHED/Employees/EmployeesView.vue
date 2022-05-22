138d8809ca0ad16c5c214defd8a7840083f35f6a
returnjosh <supremojoshua22@gmail.com>
2022-05-22 13:26:33 +0800

    update ui

--- src/views/CHED/Employees/EmployeesView.vue
+++ src/views/CHED/Employees/EmployeesView.vue
@@ -74,7 +74,14 @@
                     </div>
                 </div>
             </div>
-            <div class="flex flex-row">
+            <div class="flex flex-row justify-center items-center">
+                <div class="pr-4">
+                    <label class="hover:text-brand-lightblue hover:" @click="onUpdate()">
+                        <svg class="" style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z" />
+                        </svg>
+                    </label>
+                </div>
                 <!-- button -->
                 <div class="h-fit pt-3 items-center">
                     <button @click="excelEmployees()" type="button" class="btn-table">
@@ -306,6 +313,16 @@ export default {
         },
     },
     methods: {
+        onUpdate() {
+            this.$refs.Spinner.show();
+            this.$router.go()
+            setTimeout(
+                function () {
+                    this.$refs.Spinner.hide();
+                }.bind(this),
+                3000
+            );
+        },
         addEmployee() {
             this.$router.push("/employees/add");
         },
