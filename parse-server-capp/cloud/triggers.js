7089ce2542f4075d2412c64c0f4e51226821e732
TheToinkz <jbanday@gbox.adnu.edu.ph>
2022-05-22 08:44:17 +0800

    delete triggers done for most except users

--- parse-server-capp/cloud/triggers.js
+++ parse-server-capp/cloud/triggers.js
@@ -10,6 +10,18 @@
     }
   });
 
+  //Application Type Deletion Trigger
+  Parse.Cloud.beforeDelete("ApplicationTypes", async (request) => {
+    //Check Applications
+    const Applications = Parse.Object.extend("Applications");
+    const appQuery = new Parse.Query(Applications)
+    appQuery.equalTo("applicationTypeName", request.object.id)
+    const appCount = await appQuery.count({useMasterKey:true})
+    if (appCount > 0) {
+      throw "Can't Delete Application Type, because it is used by an Application";
+    }
+  });
+
   //Designation Deletion Trigger
   Parse.Cloud.beforeDelete("Designations", async (request) => {
     const query = new Parse.Query(Parse.User);
@@ -30,7 +42,7 @@
     }
   });
 
-  //HEI Type Deletion Trigger
+  //Discipline Deletion Trigger
   Parse.Cloud.beforeDelete("Disciplines", async (request) => {
     const query = new Parse.Query(Parse.User);
     query.equalTo("discipline", request.object.id);
@@ -53,11 +65,66 @@
     const programQuery = new Parse.Query(Programs);
     programQuery.containedIn("programDiscipline", specificDisciplines)
 
-    const progCount = await query.count({useMasterKey:true})
+    const progCount = await programQuery.count({useMasterKey:true})
     if (progCount > 0) {
       throw "Can't Delete Discipline, because it is currently linked to a Program.";
     }
   });
+
+  //Program Deletion Trigger
+  Parse.Cloud.beforeDelete("Programs", async (request) => {
+
+    //Check Applications
+    const Applications = Parse.Object.extend("Applications");
+    const appQuery = new Parse.Query(Applications)
+    appQuery.equalTo("program", request.object.id)
+    const appCount = await appQuery.count({useMasterKey:true})
+    if (appCount > 0) {
+      throw "Can't Delete Program, because it is used in an Application";
+    }
+
+    //Check Evaluation Instruments
+    const EvalInsts = Parse.Object.extend("EvaluationInstruments");
+    const evalInstQuery = new Parse.Query(EvalInsts)
+    evalInstQuery.equalTo("program", request.object.id)
+    const evalInstCount = await evalInstQuery.count({useMasterKey:true})
+    if (evalInstCount > 0) {
+      throw "Can't Delete Program, because it is used in an Evaluation Instrument";
+    }
+  });
+
+  //CHED MEMO Deletion Trigger
+  Parse.Cloud.beforeDelete("CHED_MEMO", async (request) => {
+
+    //Check Evaluation Instruments
+    const EvalInsts = Parse.Object.extend("EvaluationInstruments");
+    const evalInstQuery = new Parse.Query(EvalInsts)
+    evalInstQuery.equalTo("evalInstReqs", request.object.id)
+    const evalInstCount = await evalInstQuery.count({useMasterKey:true})
+    if (evalInstCount > 0) {
+      throw "Can't Delete CHED MEMORANDUM, because it is used in an Evaluation Instrument";
+    }
+  });
+
+  //Evaluation Instruments Deletion Trigger
+  Parse.Cloud.beforeDelete("CHED_MEMO", async (request) => {
+
+    //Check Evaluation Instruments
+    const EvalInsts = Parse.Object.extend("EvaluationInstruments");
+    const evalInstQuery = new Parse.Query(EvalInsts);
+    evalInstQuery.equalTo("objectId", request.object.id);
+    const evalInstData = await evalInstQuery.first();
+
+    const Applications = Parse.Object.extend("Applications");
+    const appQuery = new Parse.Query(Applications)
+    appQuery.equalTo("program", evalInstData.get("evaluationFormProgram"))
+    const appCount = await appQuery.count({useMasterKey:true})
+    if (appCount > 0) {
+      throw "Can't Delete Evaluation Instrument, because it is used by a Program used in an Application";
+    }
+  });
+
+
 
   
 
