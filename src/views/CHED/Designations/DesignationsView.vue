0bda6f72197dd1a08e89ca7d0db8b579bb51e899
TheToinkz <jbanday@gbox.adnu.edu.ph>
2022-05-31 22:28:24 +0800

    designations fixed

--- src/views/CHED/Designations/DesignationsView.vue
+++ src/views/CHED/Designations/DesignationsView.vue
@@ -13,7 +13,7 @@
         </div>
         <!-- button -->
         <div class="items-center">
-            <label @clieck="reset()" type="button" for="createDesignation" class="flex items-center text-white bg-brand-darkblue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 focus:outline-none">
+            <label type="button" for="createDesignation" class="flex items-center text-white bg-brand-darkblue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 focus:outline-none">
                 <svg style="fill: white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                     <path fill="none" d="M0 0h24v24H0z" />
                     <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11H7v2h4v4h2v-4h4v-2h-4V7h-2v4z" />
@@ -22,6 +22,42 @@
             </label>
         </div>
     </div>
+
+    <input type="checkbox" id="createDesignation" class="modal-toggle" />
+    <label for="createDesignation" class="modal cursor-pointer">
+        <div class="modal-box relative rounded-md text-left">
+            <div class="font-semibold text-md">ADD DESIGNATION</div>
+            <p class="py-2 text-sm">Input the name of the Designation.</p>
+            <form v-on:submit.prevent="submit">
+                <div class="mb-6">
+                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900">Designation:
+                    </label>
+                    <input type="text" id="base-input" :class="{ 'input-error': validationStatus(v$.designationName) }" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter Name" v-model="v$.designationName.$model" />
+                </div>
+            </form>
+            <div class="modal-action">
+                <label for="createDesignation" class="btn btn-sm rounded-md text-blue-700 bg-transparent border border-blue-700 hover:bg-white">Cancel</label>
+                <label for="my-modal-6" id="my-modal-6" type="submit" class="btn btn-sm bg-blue-700 rounded-md hover:bg-blue-800 border-none" @click="modal()">Submit</label>
+            </div>
+        </div>
+    </label>
+
+    <VueInstantLoadingSpinner ref="Spinner" color="#0E3385" spinnerStyle="pulse-loader" margin="4px" size="20px"></VueInstantLoadingSpinner>
+    <div :class="{ 'modal-open ': validate() }" class="modal">
+        <div class="modal-box relative rounded-md text-left">
+            <div class="font-semibold">
+                Add Designation
+            </div>
+            <p class="text-sm xxs:leading-tight text-grey-200">
+                Are you sure you want to add this designation?
+            </p>
+            <div class="modal-action">
+                <label for="my-modal-6" class="btn btn-sm rounded-md text-blue-700 bg-transparent border border-blue-700 hover:bg-white" @click="modal()">Cancel</label>
+                <label for="my-modal-6" class="btn btn-sm bg-blue-700 rounded-md hover:bg-blue-800 border-none" @click="addDesignation()">Continue</label>
+            </div>
+        </div>
+    </div>
+
 </div>
 <div v-else class="p-3">
     <!-- Table -->
@@ -176,19 +212,36 @@
                 <div class="mb-6">
                     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900">Designation:
                     </label>
-                    <input type="text" id="base-input" :class="{ 'input-error': validationStatus(v$.designationName) }" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter Name" v-model="v$.designationName.$model" />
+                    <input type="text" id="base-input" :class="{ 'input-error': validationStatus(v$.designationNameEdit) }" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter Name" v-model="v$.designationNameEdit.$model" />
                 </div>
             </form>
             <div class="modal-action">
                 <label for="editDesignation" class="btn btn-sm rounded-md text-blue-700 bg-transparent border border-blue-700 hover:bg-white" @click="reset()">
                     Cancel
                 </label>
-                <label for="my-modal-6" id="my-modal-6" type="submit" class="btn btn-sm bg-blue-700 rounded-md hover:bg-blue-800 border-none" @click="modal()">
+                <label for="my-modal-6" id="my-modal-6" type="submit" class="btn btn-sm bg-blue-700 rounded-md hover:bg-blue-800 border-none" @click="modal2()">
                     Update
                 </label>
             </div>
         </div>
     </label>
+
+    <VueInstantLoadingSpinner ref="Spinner" color="#0E3385" spinnerStyle="pulse-loader" margin="4px" size="20px"></VueInstantLoadingSpinner>
+    <div :class="{ 'modal-open ': validate2() }" class="modal">
+        <div class="modal-box relative rounded-md text-left">
+            <div class="font-semibold">
+                Update Designation
+            </div>
+            <p class="text-sm xxs:leading-tight text-grey-200">
+                Are you sure you want to update this designation?
+            </p>
+            <div class="modal-action">
+                <label for="my-modal-6" class="btn btn-sm rounded-md text-blue-700 bg-transparent border border-blue-700 hover:bg-white" @click="modal2()">Cancel</label>
+                <label for="my-modal-6" class="btn btn-sm bg-blue-700 rounded-md hover:bg-blue-800 border-none" @click="editDesignation()">Continue</label>
+            </div>
+        </div>
+    </div>
+
     <input type="checkbox" id="deleteFunc" class="modal-toggle" />
     <div class="modal">
         <div class="modal-box relative rounded-md text-left">
@@ -230,6 +283,7 @@ export default {
         return {
             deleteDis: '',
             showModal1: false,
+            showModal2: false,
             v$: useVuelidate(),
             currentpage: 0,
             numPerPage: 10,
@@ -241,6 +295,7 @@ export default {
             search: "",
             sort_type: "Sort by Designation",
             designationName: "",
+            designationNameEdit: "",
             checkedAccessTypes: [],
             designationSelected: "",
         };
@@ -248,6 +303,9 @@ export default {
     validations() {
         return {
             designationName: {
+                required,
+            },
+            designationNameEdit: {
                 required,
             },
         };
@@ -266,7 +324,7 @@ export default {
         },
     },
     methods: {
-        reset(){
+        reset() {
             this.designationName = "";
         },
         validationStatus: function (validation) {
@@ -279,6 +337,10 @@ export default {
         validate() {
             return this.showModal1;
         },
+        validate2() {
+            console.log("test")
+            return this.showModal2;
+        },
         async selectDesignation(id) {
             this.designationSelected = id;
 
@@ -288,7 +350,7 @@ export default {
 
             const designation = await desigQuery.first();
 
-            this.designationName = designation.get("name")
+            this.designationNameEdit = designation.get("name")
         },
         async modal() {
             //For Error Checking
@@ -304,32 +366,48 @@ export default {
             }
             if (has_error < 1) {
                 this.showModal1 = !this.showModal1;
-
-                //Query Designation
-                const designations = Parse.Object.extend("Designations");
-                const desigQuery = new Parse.Query(designations);
-                desigQuery.equalTo("objectId", this.designationSelected);
-
-                const designation = await desigQuery.first();
-
-                designation.set("name", this.designationName)
-
-                //Save Designation Updates
-                designation.save({
-                        name: this.designationName.toUpperCase(),
-                    }).then(() =>
-                        toast("Designation Updated", {
-                            type: TYPE.SUCCESS,
-                            timeout: 3000,
-                            position: POSITION.TOP_RIGHT,
-                        }),
-                    ),
-
-                    // window.location.reload()
-                    setTimeout(() => {
-                        window.location.reload()
-                    }, 2000);
             }
+        },
+        async modal2() {
+            //For Error Checking
+            var has_error = 0;
+            if (this.designationNameEdit == "") {
+                toast("Please fill out the required information", {
+                    type: TYPE.ERROR,
+                    timeout: 3000,
+                    hideProgressBar: true,
+                    position: POSITION.TOP_RIGHT,
+                });
+                has_error = 1;
+            }
+            if (has_error < 1) {
+                this.showModal2 = !this.showModal2;
+            }
+        },
+        async editDesignation() {
+            //Query Designation
+            const designations = Parse.Object.extend("Designations");
+            const desigQuery = new Parse.Query(designations);
+            desigQuery.equalTo("objectId", this.designationSelected);
+
+            const designation = await desigQuery.first();
+            designation.set("name", this.designationNameEdit)
+
+            //Save Designation Updates
+            designation.save({
+                    name: this.designationNameEdit.toUpperCase(),
+                }).then(() =>
+                    toast("Designation Updated", {
+                        type: TYPE.SUCCESS,
+                        timeout: 3000,
+                        position: POSITION.TOP_RIGHT,
+                    }),
+                ),
+
+                // window.location.reload()
+                setTimeout(() => {
+                    window.location.reload()
+                }, 2000);
         },
         selectedDesignationDelete(id) {
             this.deleteDis = id;
