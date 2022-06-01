0cd43a8fb9c8e548588b51f6da147aaeaf3e54bf
returnjosh <supremojoshua22@gmail.com>
2022-06-01 10:35:05 +0800

    update password

--- src/views/CHED/AccountDetails.vue
+++ src/views/CHED/AccountDetails.vue
@@ -2,125 +2,124 @@
 <div class="main-page flex justify-center items-center p-5 mx-3">
     <div class="space-y-2">
         <div class="p-4 w-full bg-white rounded-lg border border-gray-200 shadow-md">
-        <form v-on:submit.prevent="submit" class="card-body">
-            <div class="flex flex-row space-x-4 text-left justify-start items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z" />
-                </svg>
-                <span class="text-2xl font-semibold text-grey-100">UPDATE ACCOUNT</span>
-            </div>
-            <div class="line"></div>
-            <div class="flex flex-row">
-                <div class="form-control w-full pr-4">
+            <form v-on:submit.prevent="submit" class="card-body">
+                <div class="flex flex-row space-x-4 text-left justify-start items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z" />
+                    </svg>
+                    <span class="text-2xl font-semibold text-grey-100">UPDATE ACCOUNT</span>
+                </div>
+                <div class="line"></div>
+                <div class="flex flex-row">
+                    <div class="form-control w-full pr-4">
+                        <label class="label">
+                            <span class="label-text">Last Name</span>
+                        </label>
+                        <input type="text" placeholder="Last Name" :class="{ 'input-error': validationStatus(v$.lastname) }" class="input input-bordered w-full" v-model="v$.lastname.$model" />
+                        <label class="label">
+                            <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.lastname) }" v-if="validationStatus(v$.lastname)">
+                                Lastname is Required</span>
+                        </label>
+                    </div>
+
+                    <div class="form-control w-full pr-4">
+                        <label class="label">
+                            <span class="label-text">First Name</span>
+                        </label>
+                        <input type="text" placeholder="First Name" :class="{ 'input-error': validationStatus(v$.firstname) }" class="input input-bordered w-full" v-model="v$.firstname.$model" />
+                        <label class="label">
+                            <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.firstname) }" v-if="validationStatus(v$.firstname)">
+                                Firstname is Required</span>
+                        </label>
+                    </div>
+
+                    <div class="form-control" style="width: 200px">
+                        <label class="label">
+                            <span class="label-text">M.I.</span>
+                        </label>
+                        <input type="text" placeholder="M.I." :class="{ 'input-error': validationStatus(v$.midinit) }" class="input input-bordered w-full" v-model="v$.midinit.$model" />
+                        <label class="label">
+                            <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.midinit) }" v-if="validationStatus(v$.midinit)">
+                                Required</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form-control w-full">
                     <label class="label">
-                        <span class="label-text">Last Name</span>
+                        <span class="label-text">Username</span>
                     </label>
-                    <input type="text" placeholder="Last Name" :class="{ 'input-error': validationStatus(v$.lastname) }" class="input input-bordered w-full" v-model="v$.lastname.$model" />
+                    <input type="text" placeholder="Enter username" :class="{ 'input-error': validationStatus(v$.username) }" class="input input-bordered w-full" v-model="v$.username.$model" />
                     <label class="label">
-                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.lastname) }" v-if="validationStatus(v$.lastname)">
-                            Lastname is Required</span>
+                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.username) }" v-if="validationStatus(v$.username)">
+                            Username is Required</span>
+                    </label>
+                </div>
+                <div class="form-control w-full">
+                    <label class="label">
+                        <span class="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="Enter email" :class="{ 'input-error': validationStatus(v$.email) }" class="input input-bordered w-full" v-model="v$.email.$model" />
+                    <label class="label">
+                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.email) }" v-if="validationStatus(v$.email)">
+                            Email is Required</span>
+                    </label>
+                </div>
+                <div class="form-control w-full">
+                    <label class="label">
+                        <span class="label-text">Contact Number</span>
+                    </label>
+                    <input type="text" placeholder="09*********" :class="{ 'input-error': validationStatus(v$.contactnum) }" class="input input-bordered w-full" v-model="v$.contactnum.$model" />
+                    <label class="label">
+                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.contactnum) }" v-if="validationStatus(v$.contactnum)">
+                            Contact Number is Required</span>
                     </label>
                 </div>
 
-                <div class="form-control w-full pr-4">
-                    <label class="label">
-                        <span class="label-text">First Name</span>
-                    </label>
-                    <input type="text" placeholder="First Name" :class="{ 'input-error': validationStatus(v$.firstname) }" class="input input-bordered w-full" v-model="v$.firstname.$model" />
-                    <label class="label">
-                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.firstname) }" v-if="validationStatus(v$.firstname)">
-                            Firstname is Required</span>
-                    </label>
+                <div class="flex justify-end pt-8 space-x-4">
+                    <button class="btn btn-m btn-outline" @click="$router.go(-1)">
+                        Cancel
+                    </button>
+                    <button for="my-modal-6" id="my-modal-6" type="submit" class="border-none btn btn-m submit bg-brand-darkblue hover:bg-brand-blue" @click="modal()">
+                        Update Account
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div class="p-4 w-full bg-white rounded-lg border border-gray-200 shadow-md">
+            <form v-on:submit.prevent="submit" class="card-body">
+                <div class="flex flex-row space-x-4 text-left justify-start items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z" />
+                    </svg>
+                    <span class="text-2xl font-semibold text-grey-100">UPDATE PASSWORD</span>
                 </div>
 
-                <div class="form-control" style="width: 200px">
-                    <label class="label">
-                        <span class="label-text">M.I.</span>
-                    </label>
-                    <input type="text" placeholder="M.I." :class="{ 'input-error': validationStatus(v$.midinit) }" class="input input-bordered w-full" v-model="v$.midinit.$model" />
-                    <label class="label">
-                        <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.midinit) }" v-if="validationStatus(v$.midinit)">
-                            Required</span>
-                    </label>
+                <div class="">
+                    <label for="password" class="label label-text">Current Password</label>
+                    <input type="password" id="password" class="input input-bordered w-full" placeholder="•••••••••" v-model="currPass" required>
                 </div>
-            </div>
-            <div class="form-control w-full">
-                <label class="label">
-                    <span class="label-text">Username</span>
-                </label>
-                <input type="text" placeholder="Enter username" :class="{ 'input-error': validationStatus(v$.username) }" class="input input-bordered w-full" v-model="v$.username.$model" />
-                <label class="label">
-                    <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.username) }" v-if="validationStatus(v$.username)">
-                        Username is Required</span>
-                </label>
-            </div>
-            <div class="form-control w-full">
-                <label class="label">
-                    <span class="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="Enter email" :class="{ 'input-error': validationStatus(v$.email) }" class="input input-bordered w-full" v-model="v$.email.$model" />
-                <label class="label">
-                    <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.email) }" v-if="validationStatus(v$.email)">
-                        Email is Required</span>
-                </label>
-            </div>
-            <div class="form-control w-full">
-                <label class="label">
-                    <span class="label-text">Contact Number</span>
-                </label>
-                <input type="text" placeholder="09*********" :class="{ 'input-error': validationStatus(v$.contactnum) }" class="input input-bordered w-full" v-model="v$.contactnum.$model" />
-                <label class="label">
-                    <span class="label-text-alt" :class="{ 'text-error': validationStatus(v$.contactnum) }" v-if="validationStatus(v$.contactnum)">
-                        Contact Number is Required</span>
-                </label>
-            </div>
-            
-            <div class="flex justify-end pt-8 space-x-4">
-                <button class="btn btn-m btn-outline" @click="$router.go(-1)">
-                    Cancel
-                </button>
-                <button for="my-modal-6" id="my-modal-6" type="submit" class="border-none btn btn-m submit bg-brand-darkblue hover:bg-brand-blue" @click="modal()">
-                    Update Account
-                </button>
-            </div>
-        </form>
-    </div>
-
-    <div class="p-4 w-full bg-white rounded-lg border border-gray-200 shadow-md">
-        <form v-on:submit.prevent="submit" class="card-body">
-            <div class="flex flex-row space-x-4 text-left justify-start items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z" />
-                </svg>
-                <span class="text-2xl font-semibold text-grey-100">UPDATE PASSWORD</span>
-            </div>
-            
-            
-            <div class="">
-                <label for="password" class="label label-text">Current Password</label>
-                <input type="password" id="password" class="input input-bordered w-full" placeholder="•••••••••" v-model="currPass" required>
-            </div>
-            <hr>
-            <div class="">
-                <label for="password" class="label label-text">New Password</label>
-                <input type="password" id="password" class="input input-bordered w-full" placeholder="•••••••••" v-model="newPass" required>
-            </div>
-            <div class="">
-                <label for="confirm_password" class="label label-text">Confirm password</label>
-                <input type="password" id="confirm_password" class="input input-bordered w-full" placeholder="•••••••••" v-model="newPassConf" required>
-            </div>
-            <div class="flex justify-end pt-8 space-x-4">
-                <button class="btn btn-m btn-outline" @click="$router.go(-1)">
-                    Cancel
-                </button>
-                <button for="my-modal-6" id="my-modal-6" type="submit" class="border-none btn btn-m submit bg-brand-darkblue hover:bg-brand-blue" @click="changePassword()">
-                    Update Password
-                </button>
-            </div>
-        </form>
-    </div>
+                <hr>
+                <div class="">
+                    <label for="password" class="label label-text">New Password</label>
+                    <input type="password" id="password" class="input input-bordered w-full" placeholder="•••••••••" v-model="newPass" required>
+                </div>
+                <div class="">
+                    <label for="confirm_password" class="label label-text">Confirm password</label>
+                    <input type="password" id="confirm_password" class="input input-bordered w-full" placeholder="•••••••••" v-model="newPassConf" required>
+                </div>
+                <div class="flex justify-end pt-8 space-x-4">
+                    <button class="btn btn-m btn-outline" @click="$router.go(-1)">
+                        Cancel
+                    </button>
+                    <button for="my-modal-6" id="my-modal-6" type="submit" class="border-none btn btn-m submit bg-brand-darkblue hover:bg-brand-blue" @click="changePassword()">
+                        Update Password
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
     <VueInstantLoadingSpinner ref="Spinner" color="#0E3385" spinnerStyle="pulse-loader" margin="4px" size="20px"></VueInstantLoadingSpinner>
     <div :class="{ 'modal-open ': validate() }" class="modal modal-bottom sm:modal-middle">
@@ -292,24 +291,41 @@ export default {
         async changePassword() {
             //Get Current User
             const currentUser = Parse.User.current();
-            try{
+            try {
                 //Try to login User
                 var testLogin = await Parse.User.logIn(currentUser.get("username"), this.currPass);
                 console.log(testLogin)
                 console.log("Current Password is Correct");
                 //If verified to be the actual user, update password
-                if(this.newPass == this.newPassConf){
+                if (this.newPass == this.newPassConf) {
                     currentUser.setPassword(this.newPass)
                     console.log("Password Updated")
+                    toast("Password Updated", {
+                        type: TYPE.SUCCESS,
+                        timeout: 3000,
+                        hideProgressBar: true,
+                        position: POSITION.TOP_RIGHT,
+                    });
                     currentUser.save();
-                }
-                else {
+                } else {
+                    toast("New Password and Confirm New Password doesn't match", {
+                        type: TYPE.WARNING,
+                        timeout: 3000,
+                        hideProgressBar: true,
+                        position: POSITION.TOP_RIGHT,
+                    });
                     console.log("New Password and Confirm New Password doesn't match")
                 }
-            } catch(error) {
+            } catch (error) {
+                toast("Error: " + error, {
+                        type: TYPE.WARNING,
+                        timeout: 3000,
+                        hideProgressBar: true,
+                        position: POSITION.TOP_RIGHT,
+                    });
                 console.log("Current Password is wrong");
             }
-           
+
         },
         modal() {
             var has_error = 0;
@@ -398,7 +414,6 @@ export default {
                     title: queryResultDiscipline[z].get("MajorDiscipline"),
                 });
             }
-
 
             this.access_type = emp.get("access_type");
             this.emp_designation = emp.get("designation");
